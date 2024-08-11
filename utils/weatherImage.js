36b3a5dce8758389d827d818be64640c2dacbf84
sohseyedi <sohseyedi@gmail.com>
2024-08-11 10:28:41 +0330

    helper weather images

---
+++ utils/weatherImage.js
@@ -0,0 +1,18 @@
+export const weatherImage = {
+  "Partly Cloudy": require("../assets/images/partlycloudy.png"),
+  "Sunny": require("../assets/images/sun.png"),
+  "Clear": require("../assets/images/sun.png"),
+  "Overcast": require("../assets/images/cloud.png"),
+  "Cloudy": require("../assets/images/cloud.png"),
+  "Moderate rain": require("../assets/images/moderaterain.png"),
+  "Patchy rain nearby": require("../assets/images/moderaterain.png"),
+  "Patchy light drizzle":require("../assets/images/moderaterain.png"),
+  "Moderate rain at times": require("../assets/images/moderaterain.png"),
+  "Light rain": require("../assets/images/moderaterain.png"),
+  "other": require("../assets/images/moderaterain.png"),
+  "Heavy rain": require("../assets/images/heavyrain.png"),
+  "Heavy rain at times": require("../assets/images/heavyrain.png"),
+  "Moderate or freezing rain": require("../assets/images/heavyrain.png"),
+  "Moderate or heavy rain shower": require("../assets/images/heavyrain.png"),
+  "Moderate or heavy with thunder": require("../assets/images/heavyrain.png"),
+};
