dcadfe2be41729e1e517eb985d457b81ab4b0959
sohseyedi <sohseyedi@gmail.com>
2024-08-11 11:00:51 +0330

    services api

---
+++ api/https.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { API_KEY } from "@env";
+
+const BASE_URL = "https://api.weatherapi.com/v1";
+
+// fetchWeatherForecast function
+export async function fetchWeatherForecast(value, days) {
+  return await axios
+    .get(
+      `${BASE_URL}/forecast.json?key=${API_KEY}&q=${value}&days=${days}&aqi=no&alerts=no`
+    )
+    .then((response) => response.data)
+    .catch((err) => console.log(`Error fetch`, err));
+}
+
+// fetchLocations function
+
+export async function fetchLocations(value) {
+  return await axios
+    .get(`${BASE_URL}/search.json?key=${API_KEY}&q=${value}`)
+    .then((response) => response.data)
+    .catch((err) => console.log(`Error location`, err));
+}
