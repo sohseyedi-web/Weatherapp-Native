729860da0ef4e004d1e6bcc0c8fc577978ec6b05
sohseyedi <sohseyedi@gmail.com>
2024-08-12 10:15:29 +0330

    bug fixed

--- utils/toPersianNumber.js
+++ utils/toPersianNumber.js
@@ -1,5 +1,5 @@
 const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
 
 export function toPersianNumbers(n) {
-  return n.toString().replace(/\d/g, (x) => farsiDigits[parseInt(x)]);
+  return n?.toString().replace(/\d/g, (x) => farsiDigits[parseInt(x)]);
 }
