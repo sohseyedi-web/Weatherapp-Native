b0ba8b744279f980b8805fc09cd050c2974e8055
sohseyedi <sohseyedi@gmail.com>
2024-08-12 10:39:19 +0330

    create about screen navigation

--- navigation/AppNavigation.jsx
+++ navigation/AppNavigation.jsx
@@ -1,16 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
+import AboutScreen from "../screens/AboutScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
   return (
-    <NavigationContainer >
+    <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="home"
+          name="Home"
           component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
